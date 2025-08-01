4d26503a239b92f945ac21561a289444a55206b0
Muhammad Abubakar <abubakarnawaz032@gmail.com>
2025-08-01 17:25:39 +0500

    aaa

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,7 +1,8 @@
 // utils/socket.ts
 import { io } from "socket.io-client";
+const SOCKET_URL =  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:6363";
 
-const socket = io("http://localhost:6363", {
+const socket = io(SOCKET_URL, {
   transports: ["websocket"],
   autoConnect: false, // 🚫 important: disable auto connect
   reconnection: false,
