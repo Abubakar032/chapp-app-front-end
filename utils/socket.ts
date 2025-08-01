2bd2d3d2493357c1a40f26fa516796f074dc7518
Muhammad Abubakar <abubakarnawaz032@gmail.com>
2025-08-01 20:05:45 +0500

    aa

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,10 +1,11 @@
 // utils/socket.ts
 import { io } from "socket.io-client";
 import dotenv from "dotenv";
+import { Base_Url } from "@/app/config/configUrl";
 dotenv.config();
 const SOCKET_URL =  process.env.REACT_APP_BASE_URL || "http://localhost:6363";
 
-const socket = io(SOCKET_URL, {
+const socket = io(Base_Url, {
   transports: ["websocket"],
   autoConnect: false, // 🚫 important: disable auto connect
   reconnection: false,
